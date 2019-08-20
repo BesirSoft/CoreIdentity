bad74a08d0d8049635552260e2da634cb2ff6cae
alieryaman <postauyelik@gmail.com>
2019-08-20 11:37:14 +0300

    Admin controlleer ayarlandı

--- CoreIdentity/CoreIdentity/Controllers/AdminController.cs
+++ CoreIdentity/CoreIdentity/Controllers/AdminController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreIdentity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreIdentity.Controllers
 {
     public class AdminController : Controller
     {
+        private UserManager<AplicationUser> userManger;
+
+        public AdminController(UserManager<AplicationUser> _userManger)
+        {
+            userManger = _userManger;
+        }
         public IActionResult Index()
         {
-            return View();
+
+           
+
+            return View(userManger.Users);
         }
     }
 }
