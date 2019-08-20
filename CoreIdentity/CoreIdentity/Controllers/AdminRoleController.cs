bb32766cc28161a21f8ee9853c4dc1caa71d31c6
Besir <udemiyhesap@gmail.com>
2019-08-21 01:50:40 +0300

    Kullanı rolleri oluşturuldu

---
+++ CoreIdentity/CoreIdentity/Controllers/AdminRoleController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreIdentity.Controllers
+{
+    public class AdminRoleController : Controller
+    {
+
+        private RoleManager<IdentityRole> rolManager;
+
+        public AdminRoleController(RoleManager<IdentityRole> _rolManager)
+        {
+            rolManager = _rolManager;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(rolManager.Roles);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task< IActionResult> Create(string Rolname)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var result = await rolManager.CreateAsync(new IdentityRole(Rolname));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+
+                }
+
+            }
+            return View(Rolname);
+        }
+
+
+
+
+    }
+}
