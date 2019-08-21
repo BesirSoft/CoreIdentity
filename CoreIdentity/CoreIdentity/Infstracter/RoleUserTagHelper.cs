003e15e459afedfa9c5eb533e36bd38ac7cafbad
alieryaman <postauyelik@gmail.com>
2019-08-21 10:30:21 +0300

    Roler eklenme işlemi yapıldı

---
+++ CoreIdentity/CoreIdentity/Infstracter/RoleUserTagHelper.cs
@@ -0,0 +1,55 @@
+﻿using CoreIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Azure.KeyVault.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreIdentity.Infstracter
+{
+
+
+
+    [HtmlTargetElement("td", Attributes = "identity-role")]
+    public class RoleUsersTagHelper : TagHelper
+    {
+        private UserManager<AplicationUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public RoleUsersTagHelper(UserManager<AplicationUser> _userManager, RoleManager<IdentityRole> _roleManager)
+        {
+            userManager = _userManager;
+            roleManager = _roleManager;
+        }
+
+        [HtmlAttributeName("identity-role")]
+        public string Role { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> names = new List<string>();
+
+            var role = await roleManager.FindByIdAsync(Role);
+
+            if (role != null)
+            {
+                foreach (var user in userManager.Users)
+                {
+                    if (user != null && await userManager.IsInRoleAsync(user, role.Name))
+                    {
+                        names.Add(user.UserName);
+                    }
+                }
+            }
+
+            output.Content.SetContent(names.Count == 0 ?
+                "No User" :
+                string.Join(", ", names));
+        }
+
+    }
+}
+
+
